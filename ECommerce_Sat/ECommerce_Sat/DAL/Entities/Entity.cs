08e0d2adb1b9e991c868fa70a183a51276fd8487
Santiagonohaba@gmail.com <Santiagonohaba@gmail.com>
2023-04-01 17:52:05 -0500

    New CRUD CountriesController

--- ECommerce_Sat/ECommerce_Sat/DAL/Entities/Entity.cs
+++ ECommerce_Sat/ECommerce_Sat/DAL/Entities/Entity.cs
@@ -7,7 +7,11 @@ namespace ECommerce_Sat.DAL.Entities
     {
         [Key]
         public Guid Id { get; set; }
+        
+        [Display(Name = "Fecha de creación")]
         public virtual string? CreatedDate { get; set; }
+        
+        [Display(Name = "Fecha de modificación")]
         public virtual string? ModifiedDate { get; set; }
     }
 }
